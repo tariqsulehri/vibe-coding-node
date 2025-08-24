657ee7871a3cf0fa20ce039b76ecc216a936d074
tariqsulehri <tariq_sulehri@hotmail.com>
2025-08-24 20:27:43 +0500

    updated dynamic workflows -  v4

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ import logger from './utils/logger.js';
 const PORT = process.env.PORT || 4000;
 
 // added comments to check github actions - updated: 22-08-2025 T03:20PM
-console.log('Starting server...updated - updated dynamic workflows 3');
+console.log('Starting server...updated - updated dynamic workflows 4');
 async function start() {
   try {
     await connectDB(process.env.MONGO_URI);
